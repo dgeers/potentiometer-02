1f1a635ca47e631a03b42975fbd2b0ade86f3eb5
Douglas Geers <dgeers@brooklyn.cuny.edu>
2020-11-15 17:37:00 -0500

    Made a list of notes to play.

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
+let list = [60, 64, 67, 72, 70, 69, 67, 64]
 basic.forever(function () {
-    basic.pause(500)
-    music.playTone(Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, 1, 10) * 12, music.beat(BeatFraction.Whole))
+    for (let index = 0; index <= Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, 1, 8); index++) {
+        music.playTone(list[0], music.beat(BeatFraction.Whole))
+    }
 })
