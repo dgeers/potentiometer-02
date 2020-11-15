fa4f53a66eca12edf5644ffea4816b1081f233a9
Douglas Geers <dgeers@brooklyn.cuny.edu>
2020-11-15 17:40:47 -0500

    replaced MIDI pitches with frequencies

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-let list = [60, 64, 67, 72, 70, 69, 67, 64]
+let list = [262, 330, 392, 523, 494, 440, 392, 330]
 basic.forever(function () {
     for (let index = 0; index <= Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, 1, 8); index++) {
-        music.playTone(list[0], music.beat(BeatFraction.Whole))
+        music.playTone(list[index], music.beat(BeatFraction.Whole))
     }
 })
