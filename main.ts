1628afb74b52db08184dee52ef709ddbd75eb7d0
Douglas Geers <dgeers@brooklyn.cuny.edu>
2020-11-15 18:11:37 -0500

    Improved the logic with button A

--- main.ts
+++ main.ts
@@ -1,15 +1,19 @@
 input.onButtonPressed(Button.A, function () {
     if (playing == 1) {
-        for (let index = 0; index <= Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, 1, 8); index++) {
-            music.playTone(list[index], music.beat(BeatFraction.Quarter))
-        }
-        playing = 1
-    } else {
         playing = 0
+    } else {
+        playing = 1
     }
 })
-let list: number[] = []
 let playing = 0
 music.setVolume(60)
 playing = 0
-list = [262, 330, 392, 523, 494, 440, 392, 330]
+let list = [262, 330, 392, 523, 494, 440, 392, 330]
+basic.forever(function () {
+    if (playing == 1) {
+        for (let index = 0; index <= Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, 1, 8); index++) {
+            music.playTone(list[index], music.beat(BeatFraction.Quarter))
+        }
+        playing = 0
+    }
+})
