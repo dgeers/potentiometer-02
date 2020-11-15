8fe9dc2de789c66481a5523681f53c65ddd316c4
Douglas Geers <dgeers@brooklyn.cuny.edu>
2020-11-15 18:15:40 -0500

    Fixed silly mistake in previous version.

--- main.ts
+++ main.ts
@@ -14,6 +14,5 @@ basic.forever(function () {
         for (let index = 0; index <= Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, 1, 8); index++) {
             music.playTone(list[index], music.beat(BeatFraction.Quarter))
         }
-        playing = 0
     }
 })
