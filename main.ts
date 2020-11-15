ca95dbb080834ca214fbe73f7aea33f6a60522cf
Douglas Geers <dgeers@brooklyn.cuny.edu>
2020-11-15 18:03:58 -0500

    Kind of like Phil Glass...

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 let list = [262, 330, 392, 523, 494, 440, 392, 330]
 basic.forever(function () {
     for (let index = 0; index <= Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, 1, 8); index++) {
-        music.playTone(list[index], 1 / index)
+        music.playTone(list[index], music.beat(BeatFraction.Quarter))
     }
 })
