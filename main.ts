9e122247e7bceb0fc6c1a451a1391cde3062ca68
Douglas Geers <dgeers@brooklyn.cuny.edu>
2020-11-15 18:08:39 -0500

    added on/off via A button

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
-let list = [262, 330, 392, 523, 494, 440, 392, 330]
-basic.forever(function () {
-    for (let index = 0; index <= Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, 1, 8); index++) {
-        music.playTone(list[index], music.beat(BeatFraction.Quarter))
+input.onButtonPressed(Button.A, function () {
+    if (playing == 1) {
+        for (let index = 0; index <= Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, 1, 8); index++) {
+            music.playTone(list[index], music.beat(BeatFraction.Quarter))
+        }
+        playing = 1
+    } else {
+        playing = 0
     }
 })
+let list: number[] = []
+let playing = 0
+music.setVolume(60)
+playing = 0
+list = [262, 330, 392, 523, 494, 440, 392, 330]
