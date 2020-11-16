ecd4c81917b3af1749a3f6d9a5e9a0511527feee
Douglas Geers <dgeers@brooklyn.cuny.edu>
2020-11-15 22:46:09 -0500

    Added using button B to lower pitch range

--- main.ts
+++ main.ts
@@ -5,14 +5,26 @@ input.onButtonPressed(Button.A, function () {
         playing = 1
     }
 })
+input.onButtonPressed(Button.B, function () {
+    if (bassrange == 1) {
+        bassrange = 4
+    } else {
+        playing = 1
+    }
+})
+let currentnote = 0
+let bassrange = 0
 let playing = 0
 music.setVolume(60)
 playing = 0
+bassrange = 1
 let list = [262, 330, 392, 523, 494, 440, 392, 330]
 basic.forever(function () {
     if (playing == 1) {
         for (let index = 0; index <= Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, 1, 8); index++) {
-            music.playTone(list[index], music.beat(BeatFraction.Quarter))
+            currentnote = list[index]
+            currentnote = currentnote / bassrange
+            music.playTone(currentnote, music.beat(BeatFraction.Quarter))
         }
     }
 })
